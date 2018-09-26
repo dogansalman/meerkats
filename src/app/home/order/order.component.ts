74315c70d4677a43ba24f6f1e3d5722ad13bf05c
dogansalman <dogan.salman@yahoo.com>
2018-09-26 17:53:14 +0300

    update order

--- src/app/home/order/order.component.ts
+++ src/app/home/order/order.component.ts
@@ -7,16 +7,16 @@ export interface PeriodicElement {
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {unit: 1,name: 'Sıcak Çikolata', price: 1.0079},
-  {unit: 1,name: 'Çay', price: 4.0026,},
-  {unit: 1,name: 'Cheesecake', price: 6.941},
-  {unit: 1,name: 'Tiremisu', price: 9.0122},
-  {unit: 1,name: 'Türk Kahvesi', price: 10.811},
-  {unit: 1,name: 'Neskafe', price: 12.0107},
-  {unit: 1,name: 'Elmalı Turta', price: 14.0067},
-  {unit: 1,name: 'Büyük Çay', price: 15.9994},
-  {unit: 1,name: 'Kuşburnu', price: 18.9984},
-  {unit: 1,name: 'Limonata', price: 20.1797},
+  {unit: 1,name: 'Sıcak Çikolata', price: 15.00},
+  {unit: 1,name: 'Çay', price: 20.00,},
+  {unit: 1,name: 'Cheesecake', price: 22.00},
+  {unit: 1,name: 'Tiremisu', price: 18.00},
+  {unit: 1,name: 'Türk Kahvesi', price: 19.00},
+  {unit: 1,name: 'Neskafe', price: 12.80},
+  {unit: 1,name: 'Elmalı Turta', price: 14.65},
+  {unit: 1,name: 'Büyük Çay', price: 15.50},
+  {unit: 1,name: 'Kuşburnu', price: 18.50},
+  {unit: 1,name: 'Limonata', price: 20.50},
 ];
 
 
@@ -29,9 +29,15 @@ const ELEMENT_DATA: PeriodicElement[] = [
 
 export class OrderComponent{
 
+  public showPriceDown = false;
+
   displayedColumns: string[] = ['unit','name', 'price','process' ];
   dataSource = ELEMENT_DATA;
 
   constructor(){}
 
+  onChangePriceDown(): void {
+    console.log('tested');
+    this.showPriceDown = this.showPriceDown ? false : true;
+  }
 }
