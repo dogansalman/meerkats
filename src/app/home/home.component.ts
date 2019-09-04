d8ef6a4f8ed41c4c1c8e5b7b316cd47145886ab6
dogansalman <dogan@platinmarket.com>
2019-09-04 23:03:13 +0300

    updates

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,21 +2,37 @@ import {AfterContentInit, Component, OnInit, ViewEncapsulation} from '@angular/c
 import {NgxSpinnerService} from 'ngx-spinner';
 import {OrderComponent} from './order/order.component';
 import {MatDialog} from '@angular/material';
-
+import {TableService} from '../models/table/table.service';
+import {Table} from '../models/table/table';
+import {Observable} from 'rxjs/internal/Observable';
+import {keyVal} from '../operators/keyVal/keyVal';
+import {tap} from 'rxjs/operators';
 
 @Component({
   selector: 'meerkats-home',
   templateUrl: './home.component.html',
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  providers: [TableService]
 })
 export class HomeComponent implements OnInit, AfterContentInit {
 
+  /* Properties  */
+  public tables: Observable<Table[]>;
+  public locations: any;
 
-  constructor(private spn: NgxSpinnerService, private dialog: MatDialog ) { }
+  constructor(private spinner: NgxSpinnerService, private dialog: MatDialog, private tableService: TableService ) { }
 
-  ngOnInit() {  }
+  ngOnInit() {
+    /* Get locations */
+    this.tableService.get().snapshotChanges().pipe(keyVal(), tap(a => this.locations = this.tableService.getLocation(a))).subscribe();
+    /* Get tables */
+    this.tables  = this.tableService.get().snapshotChanges().pipe(keyVal(), tap(() => this.spinner.hide()));
+  }
 
-  ngAfterContentInit() { this.spn.hide(); }
+  ngAfterContentInit() {
+    /* Hide spinner when empty data */
+    this.tables.subscribe(d =>  d.length === 0 ? this.spinner.hide() : null);
+  }
 
   orderModal(): void {
     const dialogRef =  this.dialog.open(OrderComponent, {panelClass: 'fullscreen'});
